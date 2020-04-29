661884b6b10a10813d130a3e931a47549795e979
Sejin Jeon <jinaidy93@gmail.com>
2020-04-30 00:28:16 +0900

    Make Spring Boot basic structure

---
+++ src/test/java/com/sejin/i18npropertiestranslator/I18nPropertiesTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejin.i18npropertiestranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nPropertiesTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
