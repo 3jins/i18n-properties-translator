726a38d3a6bdb30bf0b7583e9133158600344a02
Sejin Jeon <jinaidy93@gmail.com>
2020-05-01 22:35:36 +0900

    [#3] Introduce PropertiesTranslationService

--- src/main/java/com/sejin/i18npropertiestranslator/translator/dto/PropertiesTranslationResponseDto.java
+++ src/main/java/com/sejin/i18npropertiestranslator/translator/dto/PropertiesTranslationResponseDto.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @Getter
 @Builder
-public class PropertiesTranslationResDto {
+public class PropertiesTranslationResponseDto {
     private LanguageType languageType;
     private Map<String, String> translatedPropertiesData;
 }
