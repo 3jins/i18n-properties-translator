006f1ff4aad81eb6189ce17a0e9f24e3bd56303a
Sejin Jeon <jinaidy93@gmail.com>
2020-05-07 01:21:52 +0900

    [#4] Add download buttons

--- src/main/frontend/src/component/body/properties-translation/TranslatedPropertiesOutput.js
+++ src/main/frontend/src/component/body/properties-translation/TranslatedPropertiesOutput.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import propertiesContentContext from '../../../context/PropertiesContentContext';
 import languageTypeMap from '../../../constant/languageTypeMap';
 import './TranslatedPropertiesOutput.css';
+import download from './download-2-16.png';
 
 export default () => {
   const {propertiesTranslationResponse} = useContext(propertiesContentContext);
@@ -11,8 +12,17 @@ export default () => {
       {propertiesTranslationResponse
         .map(propertiesTranslation => (
           <div className="translation-output" key={propertiesTranslation.languageType}>
-            <p className="language-type">{languageTypeMap[propertiesTranslation.languageType]}</p>
-            <div className="translated-properties-content">{formatPropertiesDataObject(propertiesTranslation.translatedPropertiesData)}</div>
+            <p className="language-type">
+              {languageTypeMap[propertiesTranslation.languageType]}
+              <a className="download-button"
+                 download={`i18n_${propertiesTranslation.languageType}.properties`}
+                 href={`data:application/octet-stream;,${formatPropertiesDataObjectForDownload(propertiesTranslation.translatedPropertiesData)}`}>
+                <img src={download}/>
+              </a>
+            </p>
+            <div className="translated-properties-content">
+              {formatPropertiesDataObject(propertiesTranslation.translatedPropertiesData)}
+            </div>
           </div>
         ))}
     </div>
@@ -21,8 +31,13 @@ export default () => {
   const formatPropertiesDataObject = (propertiesDataObject) => Object.keys(propertiesDataObject)
     .map(propertyKey => [propertyKey, propertiesDataObject[propertyKey]].join('='))
     .map((formattedPropertyData, idx) => (
-      <span className="formatted-property-data" key={['formattedPropertyData', idx].join('-')}>{formattedPropertyData}</span>
+      <span className="formatted-property-data"
+            key={['formattedPropertyData', idx].join('-')}>{formattedPropertyData}</span>
     ));
+
+  const formatPropertiesDataObjectForDownload = (propertiesDataObject) => Object.keys(propertiesDataObject)
+    .map(propertyKey => [propertyKey, propertiesDataObject[propertyKey]].join('='))
+    .join('\n');
 
   return (
     <div className="TranslatedPropertiesOutput">
