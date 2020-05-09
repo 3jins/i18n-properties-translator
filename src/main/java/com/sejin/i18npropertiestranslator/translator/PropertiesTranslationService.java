215a56e73c8400f1c6c4c37591e7467ba0c1ef48
Sejin Jeon <jinaidy93@gmail.com>
2020-05-09 13:41:17 +0900

    [#9] Fix bugs

--- src/main/java/com/sejin/i18npropertiestranslator/translator/PropertiesTranslationService.java
+++ src/main/java/com/sejin/i18npropertiestranslator/translator/PropertiesTranslationService.java
@@ -3,13 +3,14 @@ package com.sejin.i18npropertiestranslator.translator;
 import com.sejin.i18npropertiestranslator.common.constant.LanguageType;
 import com.sejin.i18npropertiestranslator.translator.dto.PropertiesTranslationParamDto;
 import com.sejin.i18npropertiestranslator.translator.dto.PropertiesTranslationResponseDto;
+import com.sejin.i18npropertiestranslator.translator.dto.PropertyDto;
 import com.sejin.i18npropertiestranslator.translator.papago.nmt.PapagoNmtService;
 import com.sejin.i18npropertiestranslator.translator.parser.PropertiesParsingService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,19 +21,42 @@ public class PropertiesTranslationService {
 
     public List<PropertiesTranslationResponseDto> translatePropertiesData(final PropertiesTranslationParamDto paramDto) {
         final LanguageType sourceLanguageType = paramDto.getSourceLanguageType();
-        final Map<String, String> propertiesData = propertiesParsingService.parsePropertiesRawContent(paramDto.getPropertiesRawContent());
+        final List<PropertyDto> propertyList = propertiesParsingService
+                .parsePropertiesRawContent(paramDto.getPropertiesRawContent());
+
         return paramDto.getTargetLanguageTypeList().stream()
                 .map(targetLanguageType -> {
-                    final Map<String, String> translatePropertiesData = propertiesData.entrySet().stream()
-                            .collect(Collectors.toMap(
-                                    Map.Entry::getKey,
-                                    property -> papagoNmtService.translate(property.getValue(), sourceLanguageType, targetLanguageType)
-                            ));
+                    final List<PropertyDto> translatedPropertiesData = propertyList.stream()
+                            .map(propertiesParsingResult -> translatePropertyData(propertiesParsingResult, sourceLanguageType, targetLanguageType))
+                            .collect(Collectors.toList());
                     return PropertiesTranslationResponseDto.builder()
-                            .languageType(targetLanguageType)
-                            .translatedPropertiesData(translatePropertiesData)
+                            .languageType(targetLanguageType.getPapagoCode())
+                            .translatedPropertiesData(translatedPropertiesData)
                             .build();
                 })
                 .collect(Collectors.toList());
+    }
+
+    private PropertyDto translatePropertyData(PropertyDto propertiesParsingResult, LanguageType sourceLanguageType, LanguageType targetLanguageType) {
+        final String propertyKey = propertiesParsingResult.getKey();
+        final String propertyValue = propertiesParsingResult.getValue();
+        final String propertyComment = propertiesParsingResult.getComment();
+
+        if (StringUtils.isEmpty(propertyKey) && StringUtils.isEmpty(propertyValue)) {
+            return PropertyDto.builder().comment(propertyComment).build();
+        }
+        final String translatedValue = StringUtils.isEmpty(propertyValue)
+                ? ""
+                : papagoNmtService.translate(
+                propertyValue,
+                sourceLanguageType,
+                targetLanguageType
+        );
+
+        return PropertyDto.builder()
+                .key(propertyKey)
+                .value(translatedValue)
+                .comment(propertyComment)
+                .build();
     }
 }
