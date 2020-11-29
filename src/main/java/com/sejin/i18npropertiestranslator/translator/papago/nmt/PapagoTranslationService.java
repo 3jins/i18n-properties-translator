868aedcb40fa1738e8a9823f41e898c4b478ba55
Sejin Jeon <jinaidy93@gmail.com>
2020-11-30 00:26:01 +0900

    [refactoring] Refactor HTTP response processing logic

--- src/main/java/com/sejin/i18npropertiestranslator/translator/papago/nmt/PapagoTranslationService.java
+++ src/main/java/com/sejin/i18npropertiestranslator/translator/papago/nmt/PapagoTranslationService.java
@@ -7,6 +7,7 @@ import com.sejin.i18npropertiestranslator.translator.TranslationService;
 import com.sejin.i18npropertiestranslator.translator.dto.PropertiesTranslationParsedParamDto;
 import com.sejin.i18npropertiestranslator.translator.papago.nmt.dto.PapagoTranslationRequestDto;
 import com.sejin.i18npropertiestranslator.translator.papago.nmt.dto.PapagoTranslationResponseDto;
+import com.sejin.i18npropertiestranslator.translator.papago.nmt.exception.PapagoMalfunctioningException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
@@ -42,6 +43,9 @@ public class PapagoTranslationService implements TranslationService {
                 PapagoTranslationResponseDto.class
         );
         final PapagoTranslationResponseDto papagoTranslationResponseDto = responseEntity.getBody();
-        return papagoTranslationResponseDto.getMessage().getResult().getTranslatedText();
+        if (papagoTranslationRequestDto == null) {
+            throw new PapagoMalfunctioningException();
+        }
+        return papagoTranslationResponseDto.getTranslatedText();
     }
 }
